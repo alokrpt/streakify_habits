a06002d6ca9d7f49a109ac72c464fdabeec51c7f
Alok Singh <alokrpt@gmail.com>
2024-02-05 02:20:59 +0530

    added input box for habit tracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import InputFeild from './components/InputFeild';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App: React.FC = () => {
+  const [habitName, add] = useState<string>('')
+  console.log(habitName)
+  return <div className='App'>
+    <center>
+      <h2 className="heading">
+        Streakify
+      </h2>
+      <InputFeild habitName={habitName} add={add}></InputFeild>
+    </center>
+  </div>;
 }
 
 export default App;
